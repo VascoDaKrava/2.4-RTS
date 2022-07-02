0a2e46a7c3266ecca872b9782029e881a7105367
Vasco-PC <kravchuk.vasilii@yandex.ru>
2022-07-03 01:48:38 +0300

    UniRX. Top panel. Menu
    UniRX was imported.
    Top panel with timer and menu were added.

---
+++ Assets/Scripts/Installers/CoreInstaller.cs
@@ -0,0 +1,13 @@
+﻿using Core;
+using Zenject;
+
+namespace Installers
+{
+    public class CoreInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.BindInterfacesAndSelfTo<TimeModel>().AsSingle();
+        }
+    }
+}
