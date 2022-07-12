89acf5376b3c826847586c6fcdae9c4bbae2f5df
Vasco-PC <kravchuk.vasilii@yandex.ru>
2022-07-12 17:25:45 +0300

    MoveQueue
    MoveQueue was finished

--- Assets/Scripts/Core/CommandExecutors/CommandMoveExecutor.cs
+++ Assets/Scripts/Core/CommandExecutors/CommandMoveExecutor.cs
@@ -2,6 +2,7 @@ using Abstractions;
 using Abstractions.Commands;
 using Abstractions.Commands.CommandsInterfaces;
 using System.Threading;
+using UniRx;
 using UnityEngine;
 using UnityEngine.AI;
 using AnimationState = Abstractions.AnimationState;
@@ -14,17 +15,34 @@ namespace Core.CommandExecutors
         [SerializeField] private Animator _animator;
         [SerializeField] private UnitMovementStop _stop;
         private UnitCTSource _unitCTSource;
+        private ReactiveCollection<Vector3> _queue;
 
         private void Start()
         {
             _unitCTSource = GetComponent<UnitCTSource>();
         }
 
-        public override async void ExecuteSpecificCommand(IMoveCommand command)
+        public override void ExecuteSpecificCommand(IMoveCommand command)
         {
-            _agent.destination = command.Targets[0];
+            _queue = command.Targets.ToReactiveCollection();
+            _queue.ObserveCountChanged().Subscribe(count => OnObserveCountChanged(count)).AddTo(this);
+            DoMove(_queue[0]);
+        }
+
+        private void OnObserveCountChanged(int count)
+        {
+            if (count > 0)
+            {
+                DoMove(_queue[0]);
+            }
+        }
+
+        private async void DoMove(Vector3 target)
+        {
+            _agent.destination = target;
             _animator.SetTrigger(AnimationState.Walk);
             _unitCTSource.CTSource = new CancellationTokenSource();
+
             try
             {
                 await _stop.WithCancellation(_unitCTSource.CTSource.Token);
@@ -33,10 +51,16 @@ namespace Core.CommandExecutors
             {
                 _agent.isStopped = true;
                 _agent.ResetPath();
+                _queue.Clear();
             }
 
             _unitCTSource.CTSource = null;
             _animator.SetTrigger(AnimationState.Idle);
+
+            if (_queue.Count > 0)
+            {
+                _queue.RemoveAt(0);
+            }
         }
     }
 }
